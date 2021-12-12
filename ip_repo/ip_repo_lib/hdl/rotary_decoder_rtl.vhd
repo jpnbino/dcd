a885d4a639a66a2b8608d840230becffa0406902
Joao Paulo Bino <jpnogg@gmail.com>
2021-12-12 22:38:50 +0100

    Added Rotary Decoder.

--- ip_repo/ip_repo_lib/hdl/rotary_decoder_rtl.vhd
+++ ip_repo/ip_repo_lib/hdl/rotary_decoder_rtl.vhd
@@ -12,65 +12,44 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_arith.all;
 USE ieee.std_logic_unsigned.all;
 
-entity rotary_decoder is
-  generic (
-    DataWdith : integer := 4
-  );
-  
+entity rotary_decoder is  
   port(
     clk: in std_logic;
     A: in std_logic;
     B: in std_logic;
     preset: in std_logic;
-    preset_val: in std_logic_vector(DataWdith-1 Downto 0);
-    val : out std_logic_vector(DataWdith-1 Downto 0)
+    preset_val: in std_logic_vector(4 Downto 0);
+    val : out std_logic_vector(4 Downto 0)
   );
 end entity rotary_decoder;
 
 --
 architecture rtl of rotary_decoder is
-  signal s_val: std_logic_vector(DataWdith-1 Downto 0);
-  signal cw, s_cw: std_logic;
-  signal ccw, s_ccw: std_logic;
-  
-begin
-  
-  process ( A, B )
-  begin 
-    if (rising_edge(B)) then
-      if (A = '1') then
-        cw <=  '1';
-      end if;
-    end if;
-    
-    if (rising_edge (A)) then
-      if (B = '1') then
-        ccw <= '1';
-      end if;
-    end if;
-  end process;
-
+  signal s_val: std_logic_vector(4 Downto 0);
+  signal AB: std_logic_vector (1 downto 0) := "00";
+  signal S_AB: std_logic_vector (1 downto 0) := "00";  
+begin 
   process ( clk ) 
-  begin
-    if (rising_edge(clk)) then
-      if( preset ='1') then
-        s_val <= preset_val;
-      else        
-        if ( cw ='1' and  s_cw ='0') then
-          s_val <= s_val + 1;
-          s_cw <= '1';
-        elsif ( ccw ='1' and s_ccw = '0') then
-          s_val <= s_val - 1;
-          s_ccw <= '1';
-        elsif ( ccw ='0' and cw ='0') then
-          s_cw <= '0';
-          s_ccw <= '0';
-        end if;
+  begin 
+      if (rising_edge(clk)) then
+         if( preset = '1') then
+            s_val <= preset_val;
+         else
+            if ( AB = "11" and S_AB = "10") then
+              s_val <= s_val + 1;
+              S_AB <= AB;
+            elsif ( AB = "11" and S_AB = "01") then
+              s_val <= s_val - 1;
+              S_AB <= AB;
+            else
+              S_AB <= AB;
+            end if;
+         end if;
       end if;
-    end if;
   end process;
   
 val <= s_val;
+AB <= A & B;
 
 end architecture rtl;
 
